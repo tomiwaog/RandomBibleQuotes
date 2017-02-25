22e5614f6d5f27f8f9b4e4cf762b35107022256a
tomiwaog <tommiekiddy@hotmail.co.uk>
2017-02-25 11:29:44 +0000

    Refactored code
    better naming scheme.
    
    New class - to separate data from from operations.

---
+++ 2017projects/src/randomquotes/BibleVerses.java
@@ -0,0 +1,22 @@
+package randomquotes;
+
+public final class BibleVerses {
+	
+	public static String [][] getVerses(){
+		return new String [][] {
+				{ "Do nothing from rivalry or conceit, but in humility count"
+						+ " others more significant than yourselves", "Philippians 2:3" },
+				{ "Do you see a man who is wise in his own eyes?" + "There is more hope for a fool than for him.",
+						"Proverbs 26:12" },
+				{ "Blessed is the man whom you discipline, O Lord" + "and whom you teach out of your law,\n"
+						+ "to  give him rest from days of trouble," + "until a pit is dug for the wicked.",
+						"Psalm 94:12-13" },
+				{ "And I will ask the Father, and he will give you another Helper, "
+						+ " to be with you forever, 17 even the Spirit of truth, \n"
+						+ "whom the world cannot receive, because it neither sees him nor knows him."
+						+ " You know him, for he dwells with you and will be in you.", "John 14:16-17" } 
+						};
+		
+	}
+
+}
--- 2017projects/src/randomquotes/Quotes.java
+++ 2017projects/src/randomquotes/Quotes.java
@@ -7,21 +7,9 @@ public class Quotes {
 
 	public static void main(String[] args) {
 		// 2D Array for bible verse and author page
-		String[][] bibleQuotes = {
-				{ "Do nothing from rivalry or conceit, but in humility count"
-						+ " others more significant than yourselves", "Philippians 2:3" },
-				{ "Do you see a man who is wise in his own eyes?" + "There is more hope for a fool than for him.",
-						"Proverbs 26:12" },
-				{ "Blessed is the man whom you discipline, O Lord" + "and whom you teach out of your law,\n"
-						+ "to  give him rest from days of trouble," + "until a pit is dug for the wicked.",
-						"Psalm 94:12-13" },
-				{ "And I will ask the Father, and he will give you another Helper, "
-						+ " to be with you forever, 17 even the Spirit of truth, \n"
-						+ "whom the world cannot receive, because it neither sees him nor knows him."
-						+ " You know him, for he dwells with you and will be in you.", "John 14:16-17" } };
+		String[][] bibleQuotes = BibleVerses.getVerses();
 
-		int quoteCounter = bibleQuotes.length; // Counts quote array
-
+		int verseCounter = bibleQuotes.length; // Counts quote array
 
 		//Scanner object to request input from user
 		Scanner reader = new Scanner(System.in); //
@@ -33,7 +21,7 @@ public class Quotes {
 			//Random number generator from 1 to n.
 			Random randomGen = new Random();
 			int randomNum = (Math.abs(randomGen.nextInt()));
-			randomNum %= quoteCounter;
+			randomNum %= verseCounter;
 			int quoteNum = randomNum + 1;
 			System.out.println("\n");
 			System.out.println(quoteNum + ": " + bibleQuotes[randomNum][0]);
